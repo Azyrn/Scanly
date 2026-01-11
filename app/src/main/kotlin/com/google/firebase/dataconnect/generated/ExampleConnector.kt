2ab58b24b4d56066c48a7cbb31890652f88e69c1
skeler <skeler@skeler.skeler>
2026-01-11 18:07:04 +0100

    chore: Update Firebase configuration

---
+++ app/src/main/kotlin/com/google/firebase/dataconnect/generated/ExampleConnector.kt
@@ -0,0 +1,296 @@
+
+@file:kotlin.Suppress(
+  "KotlinRedundantDiagnosticSuppress",
+  "LocalVariableName",
+  "MayBeConstant",
+  "RedundantVisibilityModifier",
+  "RemoveEmptyClassBody",
+  "SpellCheckingInspection",
+  "LocalVariableName",
+  "unused",
+)
+
+package com.google.firebase.dataconnect.generated
+
+import com.google.firebase.dataconnect.getInstance as _fdcGetInstance
+
+public interface ExampleConnector : com.google.firebase.dataconnect.generated.GeneratedConnector<ExampleConnector> {
+  override val dataConnect: com.google.firebase.dataconnect.FirebaseDataConnect
+
+  
+    public val createFolder: CreateFolderMutation
+  
+    public val createUser: CreateUserMutation
+  
+    public val getCurrentUser: GetCurrentUserQuery
+  
+    public val listDocumentsInFolder: ListDocumentsInFolderQuery
+  
+
+  public companion object {
+    @Suppress("MemberVisibilityCanBePrivate")
+    public val config: com.google.firebase.dataconnect.ConnectorConfig = com.google.firebase.dataconnect.ConnectorConfig(
+      connector = "example",
+      location = "us-east4",
+      serviceId = "scanly",
+    )
+
+    public fun getInstance(
+      dataConnect: com.google.firebase.dataconnect.FirebaseDataConnect
+    ):ExampleConnector = synchronized(instances) {
+      instances.getOrPut(dataConnect) {
+        ExampleConnectorImpl(dataConnect)
+      }
+    }
+
+    private val instances = java.util.WeakHashMap<com.google.firebase.dataconnect.FirebaseDataConnect, ExampleConnectorImpl>()
+  }
+}
+
+public val ExampleConnector.Companion.instance:ExampleConnector
+  get() = getInstance(com.google.firebase.dataconnect.FirebaseDataConnect._fdcGetInstance(config))
+
+public fun ExampleConnector.Companion.getInstance(
+  settings: com.google.firebase.dataconnect.DataConnectSettings = com.google.firebase.dataconnect.DataConnectSettings()
+):ExampleConnector =
+  getInstance(com.google.firebase.dataconnect.FirebaseDataConnect._fdcGetInstance(config, settings))
+
+public fun ExampleConnector.Companion.getInstance(
+  app: com.google.firebase.FirebaseApp,
+  settings: com.google.firebase.dataconnect.DataConnectSettings = com.google.firebase.dataconnect.DataConnectSettings()
+):ExampleConnector =
+  getInstance(com.google.firebase.dataconnect.FirebaseDataConnect._fdcGetInstance(app, config, settings))
+
+private class ExampleConnectorImpl(
+  override val dataConnect: com.google.firebase.dataconnect.FirebaseDataConnect
+) : ExampleConnector {
+  
+    override val createFolder by lazy(LazyThreadSafetyMode.PUBLICATION) {
+      CreateFolderMutationImpl(this)
+    }
+  
+    override val createUser by lazy(LazyThreadSafetyMode.PUBLICATION) {
+      CreateUserMutationImpl(this)
+    }
+  
+    override val getCurrentUser by lazy(LazyThreadSafetyMode.PUBLICATION) {
+      GetCurrentUserQueryImpl(this)
+    }
+  
+    override val listDocumentsInFolder by lazy(LazyThreadSafetyMode.PUBLICATION) {
+      ListDocumentsInFolderQueryImpl(this)
+    }
+  
+
+  @com.google.firebase.dataconnect.ExperimentalFirebaseDataConnect
+  override fun operations(): List<com.google.firebase.dataconnect.generated.GeneratedOperation<ExampleConnector, *, *>> =
+    queries() + mutations()
+
+  @com.google.firebase.dataconnect.ExperimentalFirebaseDataConnect
+  override fun mutations(): List<com.google.firebase.dataconnect.generated.GeneratedMutation<ExampleConnector, *, *>> =
+    listOf(
+      createFolder,
+        createUser,
+        
+    )
+
+  @com.google.firebase.dataconnect.ExperimentalFirebaseDataConnect
+  override fun queries(): List<com.google.firebase.dataconnect.generated.GeneratedQuery<ExampleConnector, *, *>> =
+    listOf(
+      getCurrentUser,
+        listDocumentsInFolder,
+        
+    )
+
+  @com.google.firebase.dataconnect.ExperimentalFirebaseDataConnect
+  override fun copy(dataConnect: com.google.firebase.dataconnect.FirebaseDataConnect) =
+    ExampleConnectorImpl(dataConnect)
+
+  override fun equals(other: Any?): Boolean =
+    other is ExampleConnectorImpl &&
+    other.dataConnect == dataConnect
+
+  override fun hashCode(): Int =
+    java.util.Objects.hash(
+      "ExampleConnectorImpl",
+      dataConnect,
+    )
+
+  override fun toString(): String =
+    "ExampleConnectorImpl(dataConnect=$dataConnect)"
+}
+
+
+
+private open class ExampleConnectorGeneratedQueryImpl<Data, Variables>(
+  override val connector: ExampleConnector,
+  override val operationName: String,
+  override val dataDeserializer: kotlinx.serialization.DeserializationStrategy<Data>,
+  override val variablesSerializer: kotlinx.serialization.SerializationStrategy<Variables>,
+) : com.google.firebase.dataconnect.generated.GeneratedQuery<ExampleConnector, Data, Variables> {
+
+  @com.google.firebase.dataconnect.ExperimentalFirebaseDataConnect
+  override fun copy(
+    connector: ExampleConnector,
+    operationName: String,
+    dataDeserializer: kotlinx.serialization.DeserializationStrategy<Data>,
+    variablesSerializer: kotlinx.serialization.SerializationStrategy<Variables>,
+  ) =
+    ExampleConnectorGeneratedQueryImpl(
+      connector, operationName, dataDeserializer, variablesSerializer
+    )
+
+  @com.google.firebase.dataconnect.ExperimentalFirebaseDataConnect
+  override fun <NewVariables> withVariablesSerializer(
+    variablesSerializer: kotlinx.serialization.SerializationStrategy<NewVariables>
+  ) =
+    ExampleConnectorGeneratedQueryImpl(
+      connector, operationName, dataDeserializer, variablesSerializer
+    )
+
+  @com.google.firebase.dataconnect.ExperimentalFirebaseDataConnect
+  override fun <NewData> withDataDeserializer(
+    dataDeserializer: kotlinx.serialization.DeserializationStrategy<NewData>
+  ) =
+    ExampleConnectorGeneratedQueryImpl(
+      connector, operationName, dataDeserializer, variablesSerializer
+    )
+
+  override fun equals(other: Any?): Boolean =
+    other is ExampleConnectorGeneratedQueryImpl<*,*> &&
+    other.connector == connector &&
+    other.operationName == operationName &&
+    other.dataDeserializer == dataDeserializer &&
+    other.variablesSerializer == variablesSerializer
+
+  override fun hashCode(): Int =
+    java.util.Objects.hash(
+      "ExampleConnectorGeneratedQueryImpl",
+      connector, operationName, dataDeserializer, variablesSerializer
+    )
+
+  override fun toString(): String =
+    "ExampleConnectorGeneratedQueryImpl(" +
+    "operationName=$operationName, " +
+    "dataDeserializer=$dataDeserializer, " +
+    "variablesSerializer=$variablesSerializer, " +
+    "connector=$connector)"
+}
+
+private open class ExampleConnectorGeneratedMutationImpl<Data, Variables>(
+  override val connector: ExampleConnector,
+  override val operationName: String,
+  override val dataDeserializer: kotlinx.serialization.DeserializationStrategy<Data>,
+  override val variablesSerializer: kotlinx.serialization.SerializationStrategy<Variables>,
+) : com.google.firebase.dataconnect.generated.GeneratedMutation<ExampleConnector, Data, Variables> {
+
+  @com.google.firebase.dataconnect.ExperimentalFirebaseDataConnect
+  override fun copy(
+    connector: ExampleConnector,
+    operationName: String,
+    dataDeserializer: kotlinx.serialization.DeserializationStrategy<Data>,
+    variablesSerializer: kotlinx.serialization.SerializationStrategy<Variables>,
+  ) =
+    ExampleConnectorGeneratedMutationImpl(
+      connector, operationName, dataDeserializer, variablesSerializer
+    )
+
+  @com.google.firebase.dataconnect.ExperimentalFirebaseDataConnect
+  override fun <NewVariables> withVariablesSerializer(
+    variablesSerializer: kotlinx.serialization.SerializationStrategy<NewVariables>
+  ) =
+    ExampleConnectorGeneratedMutationImpl(
+      connector, operationName, dataDeserializer, variablesSerializer
+    )
+
+  @com.google.firebase.dataconnect.ExperimentalFirebaseDataConnect
+  override fun <NewData> withDataDeserializer(
+    dataDeserializer: kotlinx.serialization.DeserializationStrategy<NewData>
+  ) =
+    ExampleConnectorGeneratedMutationImpl(
+      connector, operationName, dataDeserializer, variablesSerializer
+    )
+
+  override fun equals(other: Any?): Boolean =
+    other is ExampleConnectorGeneratedMutationImpl<*,*> &&
+    other.connector == connector &&
+    other.operationName == operationName &&
+    other.dataDeserializer == dataDeserializer &&
+    other.variablesSerializer == variablesSerializer
+
+  override fun hashCode(): Int =
+    java.util.Objects.hash(
+      "ExampleConnectorGeneratedMutationImpl",
+      connector, operationName, dataDeserializer, variablesSerializer
+    )
+
+  override fun toString(): String =
+    "ExampleConnectorGeneratedMutationImpl(" +
+    "operationName=$operationName, " +
+    "dataDeserializer=$dataDeserializer, " +
+    "variablesSerializer=$variablesSerializer, " +
+    "connector=$connector)"
+}
+
+
+
+private class CreateFolderMutationImpl(
+  connector: ExampleConnector
+):
+  CreateFolderMutation,
+  ExampleConnectorGeneratedMutationImpl<
+      CreateFolderMutation.Data,
+      CreateFolderMutation.Variables
+  >(
+    connector,
+    CreateFolderMutation.Companion.operationName,
+    CreateFolderMutation.Companion.dataDeserializer,
+    CreateFolderMutation.Companion.variablesSerializer,
+  )
+
+
+private class CreateUserMutationImpl(
+  connector: ExampleConnector
+):
+  CreateUserMutation,
+  ExampleConnectorGeneratedMutationImpl<
+      CreateUserMutation.Data,
+      CreateUserMutation.Variables
+  >(
+    connector,
+    CreateUserMutation.Companion.operationName,
+    CreateUserMutation.Companion.dataDeserializer,
+    CreateUserMutation.Companion.variablesSerializer,
+  )
+
+
+private class GetCurrentUserQueryImpl(
+  connector: ExampleConnector
+):
+  GetCurrentUserQuery,
+  ExampleConnectorGeneratedQueryImpl<
+      GetCurrentUserQuery.Data,
+      Unit
+  >(
+    connector,
+    GetCurrentUserQuery.Companion.operationName,
+    GetCurrentUserQuery.Companion.dataDeserializer,
+    GetCurrentUserQuery.Companion.variablesSerializer,
+  )
+
+
+private class ListDocumentsInFolderQueryImpl(
+  connector: ExampleConnector
+):
+  ListDocumentsInFolderQuery,
+  ExampleConnectorGeneratedQueryImpl<
+      ListDocumentsInFolderQuery.Data,
+      ListDocumentsInFolderQuery.Variables
+  >(
+    connector,
+    ListDocumentsInFolderQuery.Companion.operationName,
+    ListDocumentsInFolderQuery.Companion.dataDeserializer,
+    ListDocumentsInFolderQuery.Companion.variablesSerializer,
+  )
+
+
